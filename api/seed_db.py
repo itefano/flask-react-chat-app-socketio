3d84388053b972aed1f8a9f4a22eb2206a3a9616
solivera385 <solivera@matrice.io>
2022-04-27 12:53:04 +0200

    stuff to fix

--- api/seed_db.py
+++ api/seed_db.py
@@ -31,20 +31,21 @@ def generate_users(amount):
     users = []
     names = []
     emails = []
-    while len(names)<amount:
+    while len(names) < amount:
         firstName = fake.first_name()
         lastName = fake.last_name()
         if ((firstName, lastName) not in names):
             names.append((firstName, lastName))
-    
+
     for i in range(len(names)):
-        email = names[i][0].lower()+names[i][1].lower()+"@"+fake.safe_domain_name()
+        email = names[i][0].lower()+names[i][1].lower() + \
+            "@"+fake.safe_domain_name()
         emails.append(email)
     printProgressBar(0, amount, prefix='Generating '+str(amount)+' users...',
                      suffix='', length=50)
     for i in range(amount):
         isAdmin = False
-        if randrange(10)+1 == 0:#roughly 10% of user base will be admins
+        if randrange(10)+1 == 0:  # roughly 10% of user base will be admins
             isAdmin = True
         users.append(models.User(
             firstName=names[i][0],
@@ -59,18 +60,18 @@ def generate_users(amount):
     return users
 
 
-def generate_groups(s, amount):
+def generate_groups(s, amount):#TODO fix the amount of users in groups
     groups = []
     printProgressBar(0, amount, prefix='Generating ~'+str(int(7.5*amount))+' groups...',
                      suffix='', length=50)
     for i in range(amount):
         admin = s.query(models.User).get(i+1)
-        for ii in range (randrange(5)+5):#~5 groups created per person
+        for ii in range(randrange(5)+5):  # ~7.5 groups created per person
             participants = [admin.id]
             friends = s.query(models.Friend).filter_by(userId=admin.id).all()
             groupSize = randrange(len(friends)-1)+1
             randParticipant = randrange(amount)+1
-            for j in range(groupSize+1):
+            for j in range(groupSize+1):#TODO: fix the amounts here
                 participants.append(randParticipant)
                 while randParticipant in participants:
                     randParticipant = randrange(amount)+1
@@ -81,7 +82,8 @@ def generate_groups(s, amount):
             users = [admin]
             for k in range(len(friends)):
                 if k in participants:
-                    u = s.query(models.User).filter_by(id=friends[k].friendId).first()
+                    u = s.query(models.User).filter_by(
+                        id=friends[k].friendId).first()
                     users.append(u)
             admins = [admin]
             addedAdmin = [admin.id]
@@ -94,7 +96,7 @@ def generate_groups(s, amount):
                 name=groupName,
                 picturePath=fake.image_url(),
                 creator=admin.id,
-                admins=admins, 
+                admins=admins,
                 users=users
             ))
         printProgressBar(i + 1, amount, prefix='Generating ~'+str(int(7.5*amount))+' groups...',
@@ -108,7 +110,7 @@ def generate_friends(amount):
                      suffix='', length=50)
     for i in range(amount):
         randomFriends = []
-        for j in range(10):#each user has 10 friends
+        for j in range(10):  # each user has 10 friends
             randomFriend = randrange(amount)+1
             while randomFriend != i+1 and randomFriend in randomFriends:
                 randomFriend = randrange(amount)+1
@@ -125,7 +127,8 @@ def generate_friends(amount):
 def generate_messages(s, amount):
     messages = []
     groups = s.query(models.Group).all()
-    printProgressBar(0, len(groups), prefix='Generating ~'+str(int(7.5*len(groups)))+' messages per groups...', suffix='', length=50)
+    printProgressBar(0, len(groups), prefix='Generating ~'+str(int(5.5 *
+                     len(groups)))+' messages...', suffix='', length=50)
     for i in range(len(groups)):
         for j in range(randrange(int(amount/10))+1):
             ppath = None
@@ -141,15 +144,17 @@ def generate_messages(s, amount):
                 picturePath=ppath,
                 author=author.id,
                 groupId=group.id))
-        printProgressBar(i + 1, len(groups), prefix='Generating ~'+str(int(7.5*len(groups)))+' messages per groups...',
+        printProgressBar(i + 1, len(groups), prefix='Generating ~'+str(int(5.5*len(groups)))+' messages...',
                          suffix='', length=50)
     return messages
+
 
 def generate_notifications(s):
     messages_seen = []
     messages = s.query(models.Message).all()
     groups = s.query(models.Group).all()
-    printProgressBar(0, len(groups), prefix='Generating ~'+str(int(7.5*len(messages)))+' notifications...', suffix='', length=50)
+    printProgressBar(0, len(groups), prefix='Generating ~' +
+                     str(int(7.5*len(messages)))+' notifications...', suffix='', length=50)
     k = 0
     kk = 0
     for i in range(len(groups)):
@@ -158,7 +163,7 @@ def generate_notifications(s):
             k += 1
             r = randrange(2)
             seen = True
-            if r==0:
+            if r == 0:
                 seen = False
             for u in groups[i].users:
                 kk += 1
@@ -169,8 +174,6 @@ def generate_notifications(s):
                 ))
         printProgressBar(i + 1, len(groups), prefix='Generating ~'+str(int(7.5*len(messages)))+' notifications...',
                          suffix='', length=50)
-    print('did', k)
-    print('for total', kk)
     return messages_seen
 
 
